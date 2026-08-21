9ea9f6496e66b6c298fffb5ff54973eb25cad75b
T2xic <sniffles@bunny.burrow>
2026-08-21 18:21:43 +1000

    add args

--- grapher.py
+++ grapher.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
-"""
-Description: Graph GPU logging outputs from GPU-Z
-Usage:
-"""
-import pandas as pandas
+import pandas
 import matplotlib.pyplot as pyplot
+import argparse
 
 def trim_to_bench_start(data, idle):
     start = data[' GPU Clock [MHz] '] > idle
     return data.loc[start.idxmax():].reset_index(drop=True)
 
 if __name__ == "__main__":
-    data1 = pandas.read_csv('GPU-Z Sensor Log_bm1.txt', encoding="iso-8859-1")
-    data2 = pandas.read_csv('GPU-Z Sensor Log_bm4.txt', encoding="iso-8859-1")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("files", nargs="+", help="Filename(s) of GPU-Z log files")
+    parser.add_argument("--idle-clock", type=float, default="0", help="Trim start of graph to first row with clock speed above this value [MHz]")
+    parser.add_argument("--time", type=int, help="how many seconds of data to include in the graph")
+    parser.add_argument("--output", "-o", type=str, default="output files prefix")
+    args = parser.parse_args()
 
-    idle = 150.0
+    prefix = f"{args.output}_" if args.output else ""
 
-    data1 = trim_to_bench_start(data1, idle)
-    data2 = trim_to_bench_start(data2, idle)
+    data = []
+
+    for filename in args.files:
+        dataframe = pandas.read_csv(filename, encoding="iso-8859-1")
+        dataframe = trim_to_bench_start(dataframe, args.idle_clock)
+        data.append(dataframe)
 
     tograph = [
         ' GPU Clock [MHz] ',
@@ -28,9 +33,9 @@ if __name__ == "__main__":
         ]
 
     for i in tograph:
-        data1[i] = pandas.to_numeric(data1[i], errors='coerce')
-        data2[i] = pandas.to_numeric(data2[i], errors='coerce')
-        pyplot.plot(data1[i])
-        pyplot.plot(data2[i])
-        pyplot.savefig(f"{i}.png")
+        for dataframe in data:
+            dataframe[i] = pandas.to_numeric(dataframe[i], errors='coerce')
+            pyplot.plot(dataframe[i].iloc[:args.time])
+        filename = f"{prefix}{i.strip()}.png".replace(" ", "_")
+        pyplot.savefig(filename)
         pyplot.clf()
